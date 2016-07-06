d4cf25e9dbf1e863ec6347627056f01947f6e5d0
Grégory OBANOS <gregory.obanos@gmail.com>
2016-07-07 00:52:20 +0200

    Ajout des appels externes

--- src/lib.rs
+++ src/lib.rs
@@ -1,60 +1,76 @@
-pub enum TaskStatus {
-    TODO,
-    DONE,
+extern crate libc;
+use libc::{c_char, uint32_t};
+use std::ffi::CStr;
+use std::str;
+
+pub mod task_list;
+
+pub use task_list::*;
+
+#[no_mangle]
+pub extern fn task_list_new(name: *const c_char) -> *mut TaskList {
+    let name = unsafe {
+        assert!(!name.is_null());
+
+        CStr::from_ptr(name)
+    };
+
+    let name = name.to_str().unwrap().clone();
+
+    Box::into_raw(Box::new(TaskList::new(name.to_string())))
 }
 
-pub struct Task {
-    pub name: String,
-    pub status: TaskStatus,
-}
-
-impl Task {
-    fn new(name: String) -> Task {
-        Task {
-            name: name,
-            status: TaskStatus::TODO,
-        }
+#[no_mangle]
+pub extern fn task_list_free(list: *mut TaskList) {
+    if list.is_null() {
+        return;
     }
-
-    fn check(&mut self) {
-        self.status = TaskStatus::DONE;
-    }
-
-    fn uncheck(&mut self) {
-        self.status = TaskStatus::TODO;
+    unsafe {
+        Box::from_raw(list);
     }
 }
 
-pub struct TaskList {
-    pub name: String,
-    pub tasks: Vec<Task>,
+#[no_mangle]
+pub extern fn task_list_add(list: *mut TaskList, name: *const c_char) {
+    let list = unsafe {
+        assert!(!list.is_null());
+
+        &mut *list
+    };
+
+    let name = unsafe {
+        assert!(!name.is_null());
+
+        CStr::from_ptr(name)
+    };
+
+    let name = name.to_str().unwrap().clone();
+
+    list.add(name.to_string());
 }
 
-impl TaskList {
-    pub fn new(name: String) -> TaskList {
-        TaskList {
-            name: name,
-            tasks: vec![],
-        }
-    }
+#[no_mangle]
+pub extern fn task_list_remove(list: *mut TaskList, num: uint32_t) {
+    let list = unsafe {
+        assert!(!list.is_null());
 
-    pub fn add(&mut self, name: String) {
-        self.tasks.push(Task::new(name));
-    }
+        &mut *list
+    };
 
-    pub fn remove(&mut self, index: usize) {
-        self.tasks.remove(index);
-    }
+    let num = num as usize;
 
-    pub fn toggle(&mut self, index: usize) {
-        let mut task = self.tasks.get_mut(index).unwrap();
-        match task.status {
-            TaskStatus::TODO => {
-                task.check();
-            },
-            TaskStatus::DONE => {
-                task.uncheck();
-            }
-        }
-    }
+    list.remove(num);
+}
+
+#[no_mangle]
+pub extern fn task_list_toggle(list: *mut TaskList, num: uint32_t) {
+    let list = unsafe {
+        assert!(!list.is_null());
+
+        &mut *list
+    };
+
+    let num = num as usize;
+
+    list.toggle(num);
 }
